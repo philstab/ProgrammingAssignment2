d2b0500d505c0c651afc6126c6e8e3c030027824
Phil Stablein <pstable1@jhmi.edu>
2018-06-06 17:37:15 -0400

    philstab week3 assignment changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() creates an object for input into cacheSolve(). The 
+## makeCacheMatrix holds the original matrix argument, the inverse of
+## that matrix - if computed by cacheSolve(), and functions to mutate or 
+## access those matrix objects. Those 4 functions are returned as a list
 
-## Write a short comment describing this function
+## cacheSolve() computes the inverse of the matrix passed to makeCacheMatrix, 
+## and then accessing the functions in makeCacheMatrix(),
+##  stores that inverse for fast retrieval later.
 
+
+## takes input of a matrix, defines 4 functions for accessing or changing the 
+## matrix. Stores the inverse of the matrix after cacheSolve() is called.
 makeCacheMatrix <- function(x = matrix()) {
-
+          inverse <- NULL
+          setX<- function(passMatrix){
+                      x <<- passMatrix
+                      inverse <<- NULL
+          }
+          getX <- function() x
+          setInverse <- function(inverseObj) inverse <<- inverseObj
+          getInverse <- function()inverse
+          
+          list(setX = setX,
+               getX = getX,
+               setInverse = setInverse,
+               getInverse = getInverse
+               )
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve takes MCM object as the argument, checks to see if the inverse is 
+## stored in the makeCacheMatrix environment - if so, returns the inverse. If
+## no inverse found, it computes the inverse and stores in makeCacheMatrix obj
+cacheSolve <- function(MCM, ...) {
+        inverse <- MCM$getInverse()
+        if (!is.null(inverse)) {
+             message("retrieving cached inverted matrix")
+             return(inverse)
+        }
+        passThru <- MCM$getX()
+        inverse <- solve(passThru)
+        MCM$setInverse(inverse)
+        return(inverse)
 }
+
+#test code
+tm <- makeCacheMatrix(matrix(c(2,2,3,2), nrow = 2, ncol = 2))
+cacheSolve(tm)
+
+tm$setX(matrix(1:4, nrow=2, ncol=2))
+tm$getX()
